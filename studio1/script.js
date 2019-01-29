f247bbace5a824237e935d38d5dcb530eb0a0d76
Arushi Agarwala <aragarwala@ucdavis.edu>
2019-01-28 23:57:41 -0800

    Stylizing Mad Lib and Bob
    stylized my madlib as well as added in imagery

--- studio1/script.js
+++ studio1/script.js
@@ -10,32 +10,42 @@ function processForm(evt) {
   var liquid = document.querySelector('#liquid').value;
   var adjective = document.querySelector('#adjective').value;
   var verb1 = document.querySelector('#verb1').value;
-  var color = document.querySelector('#color').value;
+  var colr = document.querySelector('#colr').value;
   var verb2 = document.querySelector('#verb2').value;
   var noun = document.querySelector('#noun').value;
   var tool = document.querySelector('#tool').value;
   var verb3 = document.querySelector('#verb3').value;
-  var mySketch = document.querySelector('#mySketch');
+  var painting = document.querySelector('#finalProduct');
 
   var myMsg = document.querySelector('#myMsg');
-  myMsg.innerHTML = 'Start by mixing a ' + measurement + ' of ' + liquid + ' and equal parts water to create a wash for the background. Apply this wash to the canvas using ' + adjective + ' strokes. Next, take another brush, and ' + verb1 + ' it in some ' + color + ' paint. Apply this to the canvas as ' + verb2 + ' as you can. Add in ' + noun + ' with your ' + tool + '. Don’t forget; ' + verb3 + ' your painting with pride!';
+  myMsg.innerHTML = 'Start by mixing a ' + measurement + ' of ' + liquid + ' and equal parts water to create a wash for the background. Apply this wash to the canvas using ' + adjective + ' strokes. Next, take another brush, and ' + verb1 + ' it in some ' + colr + ' paint. Apply this to the canvas as ' + verb2 + ' as you can. Add in ' + noun + ' with your ' + tool + '. Don’t forget; ' + verb3 + ' your painting with pride!';
 
-  mySketch.className = 'show';
+  painting.className = 'show';
   evt.preventDefault();
 }
 
 function setup() {
-  var myCanvas = createCanvas(320, 220);
+  var myCanvas = createCanvas(350, 250);
   myCanvas.parent('mySketch');
-  img = loadImage("images/canvas.png");
-  background('#f2eadf');
 }
 
 function draw() {
-  image(img, 0, 0);
-  if (mouseIsPressed) {
-    ellipse(mouseX, mouseY, 10, 10);
-    fill(0);
-    noStroke();
+  var c = document.querySelector('#colr').value;
+  var character = c.charAt(0);
+
+  if (mouseIsPressed && mouseX >= 12 && mouseY >= 12 && mouseX <= 338 && mouseY <= 238) {
+    /* code below is interpreted from a post on Processing Forum by username: Chrisir
+       link to the post: 'https://forum.processing.org/two/discussion/17561/how-to-make-the-brush-stroke-change-based-on-the-speed-you-draw-a-line' */
+    var speed = (abs(mouseX - pmouseX) + abs(mouseY - pmouseY)) * 1.4;
+    strokeWeight(speed/3);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    /* end interpreted code */
+
+    var opacity = ((abs(mouseX - pmouseX) + abs(mouseY - pmouseY))* 10) + 20;
+    if (character == '#') {
+      stroke(c);
+    } else {
+      stroke(0, opacity);
+    }
   }
 }
